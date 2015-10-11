9998c26954ed9368fb2bcb5b38f729bb2265d284
Arseni Prokharchyk <arsenpr2011@gmail.com>
2015-10-11 13:36:27 +0300

    create initial yeoman angular project

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularTrainingApp
+ * @description
+ * # angularTrainingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularTrainingApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
